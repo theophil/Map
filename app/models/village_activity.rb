cfb2370f2219cae57fbacd936275a0b838d7d789
Your Name <tonime@andrew.cmu.edu>
2014-03-16 19:15:48 +0000

    Basically finished all

--- app/models/village_activity.rb
+++ app/models/village_activity.rb
@@ -1,2 +1,9 @@
 class VillageActivity < ActiveRecord::Base
+	#relationships
+	validates_presence_of :name
+	validates :activity_id, presence: true, numericality: { greater_than: 0, only_integer: true }
+  	validates :village_id, presence: true, numericality: { greater_than: 0, only_integer: true }
+
+  	validates_date :start_date, :on_or_after => lambda { Date.today }, :on_or_after_message => "cannot be in the past", :on => :create
+  	validates_date :end_date, :on_or_after => :start_date
 end
