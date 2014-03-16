0fb035dab9eeed7a3c68cffcb1d098437b92cbc9
Your Name <tonime@andrew.cmu.edu>
2014-03-16 20:59:49 +0000

    added destroy methods I had forgotten

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,8 +1,8 @@
 class Activity < ActiveRecord::Base
 	# #relationships
-	# has_many :village_activities
-	# has_many :villages, through: :activities
+	has_many :village_activities
+	has_many :villages, through: :village_activities
 
 	# #validations
-	# validates_presence_of :name
+	validates_presence_of :name
 end
--- test/contexts.rb
+++ test/contexts.rb
@@ -3,11 +3,25 @@ module Contexts
 		@juvadi = FactoryGirl.create(:village)
 	end
 
-	def create_village_activities
-		@va1 = FactoryGirl.create(:village_actvity, village: @village1, activity: @activity1)
+	def destroy_villages
+		@juvadi.destroy
 	end
 
 	def create_activities
 		@activity1 = FactoryGirl.create(:activity)
 	end
+
+	def destroy_activities
+		@activity1.destroy
+	end
+
+	def create_village_activities
+		@va1 = FactoryGirl.create(:village_actvity, village: @village1, activity: @activity1)
+	end
+
+	def destroy_village_activities
+		@va1.destroy
+	end
+
+	
 end
