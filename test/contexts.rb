8701ee8c2f371e2dad55f899bd5ad9013704d3c2
theophil <theophilusonime@gmail.com>
2014-03-24 17:01:04 -0400

    Update contexts.rb

--- test/contexts.rb
+++ test/contexts.rb
@@ -18,7 +18,7 @@ module Contexts
 	def create_village_activities
 		create_villages
 		create_activities
-		@va1 = FactoryGirl.create(:village_activity, village: @juvadi, activity: @activity1)
+		@va1 = FactoryGirl.create(:village_activity)
 	end
 
 	def destroy_village_activities
