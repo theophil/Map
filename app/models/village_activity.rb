1dfe9996a7ebae14883e6c41814c76a602e8f513
theophil <theophilusonime@gmail.com>
2014-03-24 16:59:30 -0400

    Update village_activity.rb

--- app/models/village_activity.rb
+++ app/models/village_activity.rb
@@ -7,7 +7,6 @@ class VillageActivity < ActiveRecord::Base
 	validates :activity_id, presence: true, numericality: { greater_than: 0, only_integer: true }
   	validates :village_id, presence: true, numericality: { greater_than: 0, only_integer: true }
   	validates_presence_of :start_date
-  	validates_presence_of :end_date
 
   	validates_date :start_date, :on_or_before => lambda { Date.today }, :on_or_after_message => "should have started in the past", :on => :create
   	validates_date :end_date, :on_or_after => :start_date
