68a6fc6150e282ea0a3fc8672b2ab07af4051a02
Theophilus Onime <tonime@andrew.cmu.edu>
2014-04-16 13:03:17 -0400

    commented out initialize method because this doesn't work how i need it to if i have initialize in

--- app/models/village.rb
+++ app/models/village.rb
@@ -10,27 +10,35 @@ class Village < ActiveRecord::Base
 
 	#scopes
 	scope :active, -> { where(active: true) }
-  	scope :inactive, -> { where(active: false) }
-  	scope :alphabetical, -> { order('name') }
+	scope :inactive, -> { where(active: false) }
+	scope :alphabetical, -> { order('name') }
 
-	#instance methods
- 	before_save :find_coordinates
+ 	
+
+  @@num_villages = 0
+
+  # def initialize
+  #   @@num_villages+=1   
+  # end
+
 
   def create_map_link(zoom=12,width=800,height=800)
-    markers = ""; marker_number = 1
+    markers = ""; marker_number = @@num_villages
     markers += "&markers=color:red%7Ccolor:red%7Clabel:#{marker_number}%7C#{self.latitude},#{self.longitude}"
     map = "http://maps.google.com/maps/api/staticmap?center= #{latitude},#{longitude}&zoom=#{zoom}&size=#{width}x#{height}&maptype=roadmap#{markers}&sensor=false"
   end
 
- 	private
-  	def find_coordinates
-  		return nil if (self.name.nil? || self.taluka.nil? || self.district.nil? || self.state.nil?)
-  		coords = Geocoder.coordinates(self.name+", "+self.taluka+", "+self.district+", "+self.state)
-  		#first index of coordinates
-  		self.latitude = coords[0]
-  		#second index of coordinates
-  		self.longitude = coords[1]
-      coords
-  	end
+  before_save :find_coordinates
+ 	
+  private
+	def find_coordinates
+		return nil if (self.name.nil? || self.taluka.nil? || self.district.nil? || self.state.nil?)
+		coords = Geocoder.coordinates(self.name+", "+self.taluka+", "+self.district+", "+self.state)
+		#first index of coordinates
+		self.latitude = coords[0]
+		#second index of coordinates
+		self.longitude = coords[1]
+    coords
+	end
 
 end
