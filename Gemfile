d71b1595d87b508089779d770b28016a12ed866a
Theophilus Onime <tonime@andrew.cmu.edu>
2014-04-30 23:27:12 -0400

    fixed routing with Gale

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source 'https://rubygems.org'
 
+gem 'cancancan'
+
 gem 'geocoder'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,8 +5,8 @@ class HomeController < ApplicationController
   	@villages = Village.active.alphabetical.to_a
   	@activities = Activity.alphabetical.active.to_a
     @village_activities = VillageActivity.all.to_a #has no active field
-    @map = create_map_link(@village_activities)
-    @marker_numbers = (1..Village.all.to_a.size).to_a
+    # @map = create_map_link(@village_activities)
+    # @marker_numbers = (1..Village.all.to_a.size).to_a
   end
 
   def about
